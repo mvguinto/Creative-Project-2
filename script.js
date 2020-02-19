6c21de0a3cfa24fa64d69f1b5d021f95f328403f
Derek Hensley <hensley.derek58@gmail.com>
2020-02-18 20:30:43 -0700

    Basic Functionality
    I have added the basci functionality to our dog website. This includes
    the html with a title, button, and a div for storing the results. The
    javascript fetches the api and creates the appropiate element to show
    the image/video. The css just resizes images and videos to 50% width and
    auto height.

---
+++ script.js
@@ -0,0 +1,39 @@
+function getDoggo() {
+  let url = "http://random.dog/woof.json"
+
+  fetch(url)
+    .then(response => {
+      if (response.status !== 200) {
+        return {
+          url: "ERROR: " + response.statusText
+        }
+      }
+      return response.json();
+    })
+    .then (json => {
+      updateDoggo(json.url)
+    });
+}
+
+function updateDoggo(url) {
+  let doggo
+  if (url.includes("ERROR")) {
+    doggo = document.createElement("P")
+    doggo.className = "Error"
+    doggo.textContent = url
+  } else if (url.includes('.mp4') || url.includes('.webm')) {
+    doggo = document.createElement("VIDEO");
+    doggo.autoplay = true;
+    doggo.loop = true;
+    doggo.controls = true;
+    doggo.src = url;
+  } else {
+    doggo = document.createElement("IMG");
+    doggo.src = url;
+  }
+  doggo.id = "doggo"
+  document.getElementById("doggo").replaceWith(doggo);
+}
+
+document.getElementById('getButton')
+  .addEventListener('click', getDoggo);
